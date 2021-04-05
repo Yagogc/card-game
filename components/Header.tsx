4ff587715b9ff860f86db3f3d25096883735f7c4
Yago Gonzalez <jagogc@gmail.com>
2021-04-05 21:18:24 +0100

    Add Header component

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header>
+      <h1 className="font-mono font-bold text-xl text-center p-4">
+        iX Card Test
+      </h1>
+    </header>
+  )
+}
+
+export { Header }
