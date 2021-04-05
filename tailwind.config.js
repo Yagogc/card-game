783716f5043b13bc4f46efef15e0c27f1519f945
Yago Gonzalez <jagogc@gmail.com>
2021-04-05 23:04:05 +0100

    Allow tailwind to purge unused styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  purge: [],
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: ['./src/**/*.html', './{components,pages}/**/*.{js,jsx,ts,tsx}'],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
